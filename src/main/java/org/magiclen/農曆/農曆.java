ec3b9cd8ffdccd94c685191045d3ed71a62d50bf
Magic Len <len@magiclen.org>
2019-08-21 23:21:58 +0800

    fix LICENSE

--- src/main/java/org/magiclen/農曆/農曆.java
+++ src/main/java/org/magiclen/農曆/農曆.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2015-2018 magiclen.org
+ * Copyright 2015 magiclen.org
  *
  * Licensed under the Apache License, 版本 2.0 (the "License");
  * you may not use this file except in compliance with the License.
